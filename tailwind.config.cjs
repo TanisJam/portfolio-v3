e01b42fad632cd45c061e054ca49aea15091cfc7
Mauricio Romero <mauricionromero@hotmail.com>
2024-08-31 14:06:47 -0300

    Update layouts and components to new design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,14 @@
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      retro_light: "#B9B99D",
+      retro_dark: "#242120",
+      retro_red: "#C14530",
+      retro_green: "#8CACB3",
+      retro_blue: "#4F83A7",
+    },
+    extend: {},
+  },
   plugins: [],
 };
